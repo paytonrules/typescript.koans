c77fcf9ebbdfaa132ac63bf335f7344a1ed274ef
davyengone <engonenzedavy@yahoo.com>
2016-10-15 23:18:04 +0200

    added few functions

---
+++ index.ts
@@ -0,0 +1,25 @@
+export function add(x: number , y : number) : number {
+    return x + y;
+}
+
+export function first(): number {
+    return 0;
+}
+
+export function last(): number {
+    return 0;
+}
+
+export function foreach(){
+
+} 
+
+export function map(){
+
+}
+
+export function reduce(){
+
+}
+
+
