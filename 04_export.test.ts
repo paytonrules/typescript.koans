87c118e2d1cb6526a3e7a525a30df58d7bdf77d3
Alexander Gugel <alexander.gugel@gmail.com>
2016-10-23 23:10:42 +0200

    Add export tests

--- 04_export.test.ts
+++ 04_export.test.ts
@@ -1,4 +1,39 @@
+import { expect } from "chai";
 import * as _ from "./04_export";
 
 describe("04_export", function () {
+  const names = [
+    "addNumbers",
+    "addStrings",
+    "identity",
+    "attempt",
+    "constant",
+    "noop",
+    "times",
+    "chunk",
+    "compact",
+    "head",
+    "initial",
+    "last",
+    "drop",
+    "dropRight",
+    "dropWhile",
+    "dropRightWhile",
+    "fill",
+    "findIndex",
+    "findLastIndex",
+    "nth",
+    "zip",
+    "forEach",
+    "every",
+    "filter",
+    "map",
+    "reduce"
+  ];
+
+  for (const name of names) {
+    it(`should export ${name}`, function () {
+      expect(typeof _[name]).to.be.equal("function");
+    });
+  }
 });
