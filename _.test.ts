afc81c2d07703eead89e0790aa6322da384d69d5
Alexander Gugel <alexander.gugel@gmail.com>
2016-10-23 19:01:27 +0100

    Restructure and cleanup

---
+++ _.test.ts
@@ -0,0 +1,165 @@
+import { expect } from "chai";
+import * as sinon from "sinon";
+import * as _ from "./_";
+
+describe("_", function () {
+  describe("addNumbers", function () {
+    it("should add two numbers", function () {
+      expect(_.addNumbers(1, 2)).to.be.equal(3);
+    });
+  });
+
+  describe("addStrings", function () {
+    it("should add two strings", function () {
+      expect(_.addStrings("hello", "world")).to.be.equal("helloworld");
+    });
+  });
+
+  describe("compact", function () {
+    it("should return array with undefined removed", function () {
+      expect(_.compact([1, undefined, 2, undefined, 3])).to.deep.equal([1, 2, 3]);
+    });
+
+    it("should return array with NaN removed", function () {
+      expect(_.compact([1, NaN, 2, NaN, 3])).to.deep.equal([1, 2, 3]);
+    });
+
+    it("should return array with null removed", function () {
+      expect(_.compact([1, null, 2, null, 3])).to.deep.equal([1, 2, 3]);
+    });
+
+    it("should return array with 0 removed", function () {
+      expect(_.compact([1, 0, 2, 0, 3])).to.deep.equal([1, 2, 3]);
+    });
+
+    it("should return array with undefined, NaN, null, 0 removed", function () {
+      expect(_.compact([1, undefined, NaN, null, 0, 2, 3])).to.deep.equal([1, 2, 3]);
+    });
+  });
+
+  describe("head", function () {
+    it("should return the first element of an array", function () {
+      expect(_.head([1, 2, 3])).to.be.equal(1);
+    });
+
+    it("should return undefined if the array is empty", function () {
+      expect(_.head([])).to.be.equal(undefined);
+    });
+  });
+
+  describe("last", function () {
+    it("should return the first element of an array", function () {
+      expect(_.last([1, 2, 3])).to.be.equal(3);
+    });
+
+    it("should return undefined if the array is empty", function () {
+      expect(_.last([])).to.be.equal(undefined);
+    });
+  });
+
+  describe("drop", function () {
+    it("should remove count items from the beginning of passed in array", function () {
+      expect(_.drop([1, 2, 3, 4], 2)).to.deep.equal([3, 4]);
+    });
+
+    it("should default to one item", function () {
+      expect(_.drop([1, 2, 3, 4])).to.deep.equal([2, 3, 4]);
+    });
+  });
+
+  describe("dropRight", function () {
+    it("should remove count items from the end of passed in array", function () {
+      expect(_.dropRight([1, 2, 3, 4], 2)).to.deep.equal([1, 2]);
+    });
+
+    it("should default to one item", function () {
+      expect(_.dropRight([1, 2, 3, 4])).to.deep.equal([1, 2, 3]);
+    });
+  });
+
+  describe("dropWhile", function () {
+    it("should ignore items until predicate returns true", function () {
+      expect(_.dropWhile([ 1, 2, 3, 4, 5, 1 ], value => value < 3)).to.deep.equal([ 3, 4, 5, 1 ]);
+    });
+  });
+
+  describe("dropRightWhile", function () {
+    it("should ignore items until predicate returns true in reversed order", function () {
+      expect(_.dropRightWhile([ 5, 4, 3, 2, 1 ], value => value < 3)).to.deep.equal([ 5, 4, 3 ]);
+    });
+  });
+
+  describe("fill", function () {
+    it("should fill array slots between start and end with value", function () {
+      expect(_.fill<any>([ 4, 6, 8, 10 ], '*', 1, 3)).to.deep.equal([ 4, '*', '*', 10 ]);
+    });
+  });
+
+  describe("findIndex", function () {
+    context("when predicate always returns false", function () {
+      it("should return -1", function () {
+        expect(_.findIndex([ 4, 6, 8, 10 ], () => false)).to.be.equal(-1);
+      });
+    });
+
+    context("when predicate returns true", function () {
+      it("should return index", function () {
+        expect(_.findIndex([ 4, 6, 8, 10 ], value => value === 6)).to.be.equal(1);
+      });
+    });
+
+    context("when startIndex > index of first match", function () {
+      it("should return index of second match", function () {
+        expect(_.findIndex([ 4, 6, 6, 8, 10 ], value => value === 6, 2)).to.be.equal(2);
+      });
+    });
+  });
+
+  describe("findLastIndex", function () {
+    context("when predicate always returns false", function () {
+      it("should return -1", function () {
+        expect(_.findLastIndex([ 4, 6, 8, 10 ], () => false)).to.be.equal(-1);
+      });
+    });
+
+    context("when predicate returns true", function () {
+      it("should return index", function () {
+        expect(_.findLastIndex([ 4, 6, 8, 10 ], value => value === 6)).to.be.equal(1);
+      });
+    });
+
+    context("when startIndex > index of first match", function () {
+      it("should return index of second match", function () {
+        expect(_.findLastIndex([ 4, 6, 6, 8, 10 ], value => value === 6, 1)).to.be.equal(1);
+      });
+    });
+  });
+
+  describe("forEach", function () {
+    context("when collection is an array", function () {
+      it("should iterate over all items of array", function () {
+        const collection = ["first", "second", "third"];
+        const iteratee = sinon.spy();
+        _.forEach(collection, iteratee);
+        sinon.assert.calledWithExactly(iteratee, "first", 0, collection);
+        sinon.assert.calledWithExactly(iteratee, "second", 1, collection);
+        sinon.assert.calledWithExactly(iteratee, "third", 2, collection);
+      });
+    });
+
+    context("when collection is an object", function () {
+      it("should iterate over all items of object", function () {
+        const collection: _.Dictionary<string> = {
+          "0": "first",
+          "1": "second",
+          "2": "third"
+        };
+        const iteratee = sinon.spy();
+        _.forEach(collection, iteratee);
+        sinon.assert.calledWithExactly(iteratee, "first", "0", collection);
+        sinon.assert.calledWithExactly(iteratee, "second", "1", collection);
+        sinon.assert.calledWithExactly(iteratee, "third", "2", collection);
+      });
+    });
+  });
+});
---
+++ queue.ts
@@ -0,0 +1,216 @@
+// # Generics
+// Let's look at a concept called "generics".
+// To fully understand what generics are and how to use them, let's first look
+// at a couple of simple examples.
+
+// ## NumberQueue
+// Let's say we want to implement a queue of numbers. We might end up writing
+// something like this:
+
+// TypeScript allows us to use interfaces. By convention we prefix them with "I",
+// but it's not strictly required.
+interface INumberQueue {
+  // enqueue() adds an item to the number queue.
+  enqueue(value: number): void;
+
+  // dequeue() remove the first item from the number queue.
+  dequeue(): number;
+
+  // toArray allows us to serialize the queue. This is useful for testing.
+  toArray(): Array<number>;
+}
+
+// INumberQueueItem is our queue item interface. Our queue is implemented in a
+// recursive fashion, thus the methods look farily similar.
+interface INumberQueueItem {
+  value: number;
+  next: INumberQueueItem;
+
+  // We use a nice trick here to avoid checking on every call of enqueue if we're
+  // the last item in the queue.
+  enqueue(value: number): INumberQueueItem;
+
+  toArray(): Array<number>;
+}
+
+// TailNumberQueueItem is the last item in the linked queue. It implements the
+// INumberQueueItem interface.
+class TailNumberQueueItem implements INumberQueueItem {
+  public value: number = null;
+
+  // The tail item's successor is the tail item itself.
+  public next: TailNumberQueueItem = this;
+
+  enqueue (value: number): NumberQueueItem {
+    // When we're the last item in the queue (which is the case here), enqueue wraps
+    // our passed in value in a NumberQueueItem and returns it.
+    return new NumberQueueItem(value);
+  }
+
+  // When the queue is empty, toArray() returns an empty array.
+  toArray (): Array<number> {
+    return [];
+  }
+}
+
+// The NumebrQueueItem wraps a value of the queue.
+class NumberQueueItem implements INumberQueueItem {
+  public value: number;
+  public next: TailNumberQueueItem = new TailNumberQueueItem();
+
+  constructor (value: number) {
+    this.value = value;
+  }
+
+  enqueue (value: number): NumberQueueItem {
+    // When we add an item to the queue and we're the last "real" queue item that
+    // holds a value, we need to update our reference to the next queue item.
+    this.next = this.next.enqueue(value);
+    return this;
+  }
+
+  toArray (): Array<number> {
+    // The time complexity of this isn't great. We could use generators for
+    // this.
+    return [this.value, ...this.next.toArray()];
+  }
+}
+
+export class NumberQueue implements INumberQueue {
+  // The head of the queue can initially be set to a new TailNumberQueueItem.
+  private head: TailNumberQueueItem = new TailNumberQueueItem();
+
+  enqueue (value: number) : void {
+    // The nice thing about the way we implemented the TailNumberQueueItem is
+    // that we don't actually have to check whether or not our queue is empty. We
+    // can always just enqueue() onto it.
+    this.head = this.head.enqueue(value);
+  }
+
+  dequeue (): number {
+    const value = this.head.value;
+    this.head = this.head.next;
+    return value;
+  }
+
+  toArray (): Array<number> {
+    // Same with toArray()...
+    return this.head.toArray();
+  }
+}
+
+// ## Queue
+// But what if instead of having a queue of numbers, we want to have a queue of
+// strings? We would either have to completely re-implement the above
+// functionality or implement it using the "any" data type.
+// Using "any" means we can no longer rely on the level of type safety that 
+// TypeScript compiler guarantees, while re-implementing it i tedious, error-
+// prone duplicates a lot of logic.
+
+// That's why we need generics! Generics allow us to define a queue (or any data
+// structure or parameter) using one of more abstract data types (often called
+// `T` or `E`).
+
+// TypeScript allows us to use interfaces. By convention we prefix them with "I",
+// but it's not strictly required.
+interface IQueue <T> {
+  // enqueue() adds an item to the number queue.
+  enqueue(value: T): void;
+
+  // dequeue() remove the first item from the number queue.
+  dequeue(): T;
+
+  // toArray allows us to serialize the queue. This is useful for testing.
+  toArray(): Array<T>;
+}
+
+// IQueueItem is our queue item interface. Our queue is implemented in a
+// recursive fashion, thus the methods look farily similar.
+interface IQueueItem <T> {
+  value: T;
+  next: IQueueItem<T>;
+
+  // We use a nice trick here to avoid checking on every call of enqueue if we're
+  // the last item in the queue.
+  enqueue(value: T): IQueueItem<T>;
+
+  toArray(): Array<T>;
+}
+
+// TailQueueItem is the last item in the linked queue. It implements the
+// IQueueItem interface.
+class TailQueueItem<T> implements IQueueItem<T> {
+  public value: T = null;
+
+  // The tail item's successor is the tail item itself.
+  public next: TailQueueItem<T> = this;
+
+  enqueue (value: T): QueueItem<T> {
+    // When we're the last item in the queue (which is the case here), enqueue wraps
+    // our passed in value in a QueueItem and returns it.
+    return new QueueItem<T>(value);
+  }
+
+  // When the queue is empty, toArray() returns an empty array.
+  toArray (): Array<T> {
+    return [];
+  }
+}
+
+// The NumebrQueueItem wraps a value of the queue.
+class QueueItem<T> implements IQueueItem<T> {
+  public value: T;
+  public next: TailQueueItem<T> = new TailQueueItem<T>();
+
+  constructor (value: T) {
+    this.value = value;
+  }
+
+  enqueue (value: T): QueueItem<T> {
+    // When we add an item to the queue and we're the last "real" queue item that
+    // holds a value, we need to update our reference to the next queue item.
+    this.next = this.next.enqueue(value);
+    return this;
+  }
+
+  toArray (): Array<T> {
+    // The time complexity of this isn't great. We could use generators for
+    // this.
+    return [this.value, ...this.next.toArray()];
+  }
+}
+
+export class Queue<T> implements IQueue<T> {
+  // The head of the queue can initially be set to a new TailQueueItem.
+  private head: TailQueueItem<T> = new TailQueueItem<T>();
+
+  enqueue (value: T) : void {
+    // The nice thing about the way we implemented the TailQueueItem is
+    // that we don't actually have to check whether or not our queue is empty. We
+    // can always just enqueue() onto it.
+    this.head = this.head.enqueue(value);
+  }
+
+  dequeue (): T {
+    const value: T = this.head.value;
+    this.head = this.head.next;
+    return value;
+  }
+
+  toArray (): Array<T> {
+    // Same with toArray()...
+    return this.head.toArray();
+  }
+}
+
+// ### Example
+// Now, in order to use our list, we can define a new queue of data type T,
+// which can be anything: `Object`, `number`, `string`, `union types`, or even
+// `any`!
+//
+// ```js
+// const queue = new Queue<string>();
+// queue.enqueue('first');
+// queue.enqueue('second');
+// queue.enqueue('third');
+// ```
