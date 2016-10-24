981f8ab056be271618c6f46c11dfad643b40d2f1
Alexander Gugel <alexander.gugel@gmail.com>
2016-10-24 16:40:53 +0200

    Add some extra tasks

---
+++ 07_extra.test.ts
@@ -0,0 +1,39 @@
+import { expect } from "chai";
+import * as sinon from "sinon";
+import * as _ from "./07_extra";
+
+describe("07_extra", function () {
+  describe("join", function () {
+    context("when called without separator", function () {
+      it("should join using \",\"", function () {
+        expect(_.join(["hello", "world", "!"])).to.be.equal("hello,world,!");
+      });
+    });
+
+    context("when called with separator", function () {
+      it("should join using separator", function () {
+        expect(_.join(["hello", "world", "!"], " beautiful ")).to.be.equal("hello beautiful world beautiful !");
+      });
+    });
+  });
+
+  describe("ary", function () {
+    context("when called with exactly n arguments", function () {
+      it("should call function with n arguments", function () {
+        const func = sinon.stub().returns("yeah");
+        expect(_.ary(func, 3)(1, 2, 3)).to.be.equal("yeah");
+        sinon.assert.callCount(func, 1);
+        sinon.assert.calledWithExactly(func, 1, 2, 3);
+      });
+    });
+
+    context("when called with > n arguments", function () {
+      it("should call function with n arguments", function () {
+        const func = sinon.stub().returns("yeah");
+        expect(_.ary(func, 3)(1, 2, 3, 4)).to.be.equal("yeah");
+        sinon.assert.callCount(func, 1);
+        sinon.assert.calledWithExactly(func, 1, 2, 3);
+      });
+    });
+  });
+});
